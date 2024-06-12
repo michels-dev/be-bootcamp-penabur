0675ed246ec09ca260c5d7e79533a63516b7a21b
michelsdev <sinambelamichel05@gmail.com>
2024-06-12 17:20:16 +0700

    Assignment ejs

--- week2/materi1-web-server/app.js
+++ week2/materi1-web-server/app.js
@@ -1,13 +1,82 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 
-app.use('/', routes);
-// port
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware untuk load data
+const loadContactsData = (req, res, next) => {
+    const dataFilePath = path.join(__dirname, 'data', 'contacts.json');
+    fs.readFile(dataFilePath, 'utf8', (err, jsonData) => {
+        if (err) {
+            return res.status(500).render('404', { data: { title: '500', message: 'Internal Server Error' } });
+        }
+        res.locals.contacts = JSON.parse(jsonData);
+        next();
+    });
+};
+
+// Route handlers
+app.get('/', (req, res) => {
+    const data = {
+        title: 'Index',
+        message: 'MAIN PAGE',
+    };
+    res.render('index', { data });
+});
+
+app.get('/about', (req, res) => {
+    const data = {
+        title: 'About',
+        message: 'PAGE ABAOUT',
+    };
+    res.render('about', { data });
+});
+
+app.get('/contact', loadContactsData, (req, res) => {
+    const data = {
+        title: 'Contact',
+        message: 'PAGE CONTACT',
+        contact: res.locals.contacts,
+    };
+    res.render('contact', { data });
+});
+
+app.get('/contact/:id', loadContactsData, (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const contact = res.locals.contacts.find(c => c.id === id);
+
+    if (!contact) {
+        const data = {
+            title: '404',
+            message: 'NOT FOUND',
+        };
+        return res.status(404).render('404', { data });
+    }
+
+    const data = {
+        title: 'Contact Details',
+        message: 'Contact Details',
+        contact: contact,
+    };
+    res.render('contact-details', { data });
+});
+
+// 404 handler
+app.use((req, res) => {
+    const data = {
+        title: '404',
+        message: 'Page Not Found',
+    };
+    res.status(404).render('404', { data });
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
--- week2/materi1-web-server/web-server.js
+++ week2/materi1-web-server/web-server.js
@@ -1,11 +1,39 @@
+const fs = require('fs');
 const http = require('http');
+const port = 3000;
+
+const renderHTML = (path, res) =>{
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            res.writeHead(404);
+            res.write('Error : page not found');
+        } else {
+            res.write(data)
+        }
+        res.end();
+    })
+}
 
 http
-.createServer((req, res) => {
-  res.write('Hello Wolrd!');
-  res.end();
-})
+    .createServer((req,res)=>{
+        const url = req.url;
+        console.log(url);
 
-.listen(3000,() => {
-  console.log('Server is listening on port 3000');
-});
+        res.writeHead(200,{
+            'Content-Type': 'text/html',
+        });
+
+        if(url==='/about'){
+            renderHTML('./about.html',res);
+        } else if(url==='/contact'){
+            renderHTML('./contact.html',res);
+        } else {
+            // res.write('Hello World!');
+            renderHTML('./index.html',res);
+        }
+
+
+    })
+    .listen(port,()=>{
+        console.log(`Server is listening on port ${port}...`);
+    });
