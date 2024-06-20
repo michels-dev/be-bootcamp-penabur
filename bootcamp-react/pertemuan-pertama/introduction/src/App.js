431ea457ed2ef35935f6b16dd9b5c6d7b2f6e34d
michelsdev <sinambelamichel05@gmail.com>
2024-06-20 13:59:32 +0700

    assignment done

--- bootcamp-react/pertemuan-pertama/introduction/src/App.js
+++ bootcamp-react/pertemuan-pertama/introduction/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+const user = {
+  name: 'React JS',
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+      <ul>
+        <h1 className='text-h1'>
+          BOOTCAMP Batch 1 : Experiment with {user.name}
+        </h1>
+
+        <li>
+          <a href="#home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#news">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+      </nav>
+      <h2 className="text-h2">Hello World!, This is {user.name}.</h2>
     </div>
   );
 }
