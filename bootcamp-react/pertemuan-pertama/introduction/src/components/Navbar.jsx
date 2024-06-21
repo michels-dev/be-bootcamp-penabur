6c907183ec68ac556f60e81203ad8cb0e02ffe29
michelsdev <sinambelamichel05@gmail.com>
2024-06-21 14:10:08 +0700

    modified assignment pertemuan 2 using prime-react

--- bootcamp-react/pertemuan-pertama/introduction/src/components/Navbar.jsx
+++ bootcamp-react/pertemuan-pertama/introduction/src/components/Navbar.jsx
@@ -1,20 +1,27 @@
-const Navbar = ({ home, menuItems}) => {
-  return (
-  <div className="navbar bg-base-100">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl">{home}</a>
-    </div>
-    <div className="flex-none">
-      <ul className="menu menu-horizontal px-1">
-            {menuItems.map((item, index) => (
-              <li key={index} className="px-5">
-                <a>{item}</a>
-              </li>
-            ))}
-          </ul>
-    </div>
-  </div>
-  )
-}
+import { Menubar } from 'primereact/menubar';
+import 'primereact/resources/themes/saga-blue/theme.css'; // Theme CSS
+import 'primereact/resources/primereact.min.css';
 
+const Navbar = ({ home, menuItems }) => {
+  const items = [
+    {
+      label: home,
+      icon: 'pi pi-home',
+    },
+    ...menuItems.map(item => (
+      {
+        label: item,
+        icon: 'pi pi-star',
+      }
+    )),
+  ];
+
+  const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" className="w-10 mr-20" />;
+
+  return (
+    <div clasName="navbar bg-base-100 flex justify-between items-center">
+      <Menubar model={items} start={start} className="mr-10 w-full" />
+    </div>
+  );
+};
 export default Navbar;
--- bootcamp-react/pertemuan-pertama/introduction/tailwind.config.js
+++ bootcamp-react/pertemuan-pertama/introduction/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
